456101ceabe6ea7f92873992cd4f455819022907
solongtony <solongtony@gmail.com>
2019-05-12 10:51:14 -0400

    three dancers!

--- stick_figure.js
+++ stick_figure.js
@@ -42,12 +42,6 @@ function svg_line(start, end) {
   return `<line x1="${start.x}" y1="${start.y}" x2="${end.x}" y2="${end.y}"/>`;
 }
 
-function extend(start, direction, body_part) {
-  return start.polar_add(
-    DIRECTIONS.get(direction) + rand_shift(),
-    BODY_LENGTHS.get(body_part));
-}
-
 function svg_ellipse(center, radii) {
   return `<ellipse cx="${center.x}" cy="${center.y}" rx="${radii.x}" ry="${radii.y}"/>`;
 }
@@ -59,14 +53,17 @@ function offset_ellipse(base, direction, xr, yr) {
   return svg_ellipse(center, new Point(xr, yr));
 }
 
-function rand_shift() {
-  return Math.random()/16 - 1/32 ;
+function rand_shift(n) {
+  return (Math.random()/16 - 1/32) * n ;
 }
 
+// Array.from(p, ([key, value]) => value * value)
+
 class StickFigure {
-  constructor(id, center_of_gravity = new Point(0, 0)) {
+  constructor(id, center_of_gravity = new Point(0, 0), scale = 1) {
     this.id = name;
     this.hip_nexus = center_of_gravity;
+    this.body_lengths = new Map(Array.from(BODY_LENGTHS, ([k, v]) => [k, v * scale]));
   }
 
   // Create svg elements to draw the stick figure.
@@ -105,8 +102,8 @@ class StickFigure {
 
   // Side Effect! Sets `shoulder_nexus`
   torso() {
-    const belly_end = extend(this.hip_nexus ,'up', 'belly');
-    const ribs_end = extend(belly_end ,'up', 'ribs');
+    const belly_end = this.extend(this.hip_nexus ,'up', 'belly');
+    const ribs_end = this.extend(belly_end ,'up', 'ribs');
     this.shoulder_nexus = ribs_end;
     return [
       svg_line(this.hip_nexus, belly_end),
@@ -115,58 +112,74 @@ class StickFigure {
   }
 
   neck_head() {
-    const neck_end = extend(this.shoulder_nexus ,'up', 'neck');
+    const neck_end = this.extend(this.shoulder_nexus ,'up', 'neck');
     return [
       svg_line(this.shoulder_nexus, neck_end),
-      offset_ellipse(neck_end, DIRECTIONS.get('up'), BODY_LENGTHS.get('head_half_width'), BODY_LENGTHS.get('head_half_height'))
+      offset_ellipse(neck_end, DIRECTIONS.get('up'), this.body_lengths.get('head_half_width'), this.body_lengths.get('head_half_height'))
     ];
   }
 
   right_arm() {
-    const shoulder_end = extend(this.shoulder_nexus ,'right', 'shoulder');
-    const upper_arm_end = extend(shoulder_end ,'down', 'upper_arm');
-    const lower_arm_end = extend(upper_arm_end ,'down', 'lower_arm');
+    const shoulder_end = this.extend(this.shoulder_nexus ,'right', 'shoulder');
+    const upper_arm_end = this.extend(shoulder_end ,'down', 'upper_arm');
+    const lower_arm_end = this.extend(upper_arm_end ,'down', 'lower_arm');
     return [
       svg_line(this.shoulder_nexus, shoulder_end),
       svg_line(shoulder_end, upper_arm_end),
       svg_line(upper_arm_end, lower_arm_end),
-      offset_ellipse(lower_arm_end, DIRECTIONS.get('down'), BODY_LENGTHS.get('hand_half_width'), BODY_LENGTHS.get('hand_half_height'))
+      offset_ellipse(lower_arm_end, DIRECTIONS.get('down'), this.body_lengths.get('hand_half_width'), this.body_lengths.get('hand_half_height'))
     ];
   }
 
   left_arm() {
-    const shoulder_end = extend(this.shoulder_nexus ,'left', 'shoulder');
-    const upper_arm_end = extend(shoulder_end ,'down', 'upper_arm');
-    const lower_arm_end = extend(upper_arm_end ,'down', 'lower_arm');
+    const shoulder_end = this.extend(this.shoulder_nexus ,'left', 'shoulder');
+    const upper_arm_end = this.extend(shoulder_end ,'down', 'upper_arm');
+    const lower_arm_end = this.extend(upper_arm_end ,'down', 'lower_arm');
     return [
       svg_line(this.shoulder_nexus, shoulder_end),
       svg_line(shoulder_end, upper_arm_end),
       svg_line(upper_arm_end, lower_arm_end),
-      offset_ellipse(lower_arm_end, DIRECTIONS.get('down'), BODY_LENGTHS.get('hand_half_width'), BODY_LENGTHS.get('hand_half_height'))
+      offset_ellipse(lower_arm_end, DIRECTIONS.get('down'), this.body_lengths.get('hand_half_width'), this.body_lengths.get('hand_half_height'))
     ];
   }
 
   right_leg() {
-    const hip_end = extend(this.hip_nexus ,'right', 'hip');
-    const upper_leg_end = extend(hip_end ,'down', 'upper_leg');
-    const lower_leg_end = extend(upper_leg_end ,'down', 'lower_leg');
+    const hip_end = this.extend(this.hip_nexus ,'right', 'hip');
+    const upper_leg_end = this.extend(hip_end ,'down', 'upper_leg');
+    const lower_leg_end = this.extend(upper_leg_end ,'down', 'lower_leg');
     return [
       svg_line(this.hip_nexus, hip_end),
       svg_line(hip_end, upper_leg_end),
       svg_line(upper_leg_end, lower_leg_end),
-      offset_ellipse(lower_leg_end, DIRECTIONS.get('down'), BODY_LENGTHS.get('foot_half_width'), BODY_LENGTHS.get('foot_half_height'))
+      offset_ellipse(lower_leg_end, DIRECTIONS.get('down'), this.body_lengths.get('foot_half_width'), this.body_lengths.get('foot_half_height'))
     ];
   }
 
   left_leg() {
-    const hip_end = extend(this.hip_nexus ,'left', 'hip');
-    const upper_leg_end = extend(hip_end ,'down', 'upper_leg');
-    const lower_leg_end = extend(upper_leg_end ,'down', 'lower_leg');
+    const hip_end = this.extend(this.hip_nexus ,'left', 'hip');
+    const upper_leg_end = this.extend(hip_end ,'down', 'upper_leg');
+    const lower_leg_end = this.extend(upper_leg_end ,'down', 'lower_leg');
     return [
       svg_line(this.hip_nexus, hip_end),
       svg_line(hip_end, upper_leg_end),
       svg_line(upper_leg_end, lower_leg_end),
-      offset_ellipse(lower_leg_end, DIRECTIONS.get('down'), BODY_LENGTHS.get('foot_half_width'), BODY_LENGTHS.get('foot_half_height'))
+      offset_ellipse(lower_leg_end, DIRECTIONS.get('down'), this.body_lengths.get('foot_half_width'), this.body_lengths.get('foot_half_height'))
     ];
+  }
+
+  extend(start, direction, body_part) {
+    // Dance!
+    // Random bend in joints.
+    // Bigger bends in arms and legs
+    // var multiplier;
+    // switch(body_part.match(/leg|arm/)[0]) {
+    //   case "arm": multiplier = 8; console.log("arm"); break;
+    //   case "leg": multiplier = 3; console.log("leg"); break;
+    //   default: multiplier = 1;
+    // }
+    // This is the actual this.extend logic.
+    return start.polar_add(
+      DIRECTIONS.get(direction) + rand_shift(4),
+      this.body_lengths.get(body_part));
   }
 }
