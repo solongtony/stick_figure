3f216e90b21c5cddc6397f71a0ec6f0a995c631b
solongtony <solongtony@gmail.com>
2019-05-12 08:13:25 -0400

    cleanup

--- stick_figure.js
+++ stick_figure.js
@@ -1,33 +1,36 @@
 // Depends on point.js
 
+// Scaling factor.
+// Determines the ration of line width figure size.
 const FACTOR = 10;
-
-// Array.from(p, ([key, value]) => value * value)
 
 // Proportions measured in heads.
 const BODY_LENGTHS = new Map([
-  ["head_half_height", 0.5 * FACTOR],
-  ["head_half_width", 0.3 * FACTOR],
-  ["neck", 0.5 * FACTOR],
-  ["ribs", 1 * FACTOR],
-  ["belly", 1 * FACTOR],
-  ["shoulder", 0.75 * FACTOR],
-  ["upper_arm", 1.25 * FACTOR],
-  ["lower_arm", 1 * FACTOR],
-  ["hand_half_height", 0.3 * FACTOR],
-  ["hand_half_width", 0.2 * FACTOR],
-  ["hip", 0.5 * FACTOR],
-  ["upper_leg", 1.5 * FACTOR],
-  ["lower_leg", 1.5 * FACTOR],
-  ["foot_half_height", 0.15 * FACTOR],
-  ["foot_half_width", 0.3 * FACTOR],
+  ['head_half_height', 0.5 * FACTOR],
+  ['head_half_width', 0.3 * FACTOR],
+  ['neck', 0.5 * FACTOR],
+  ['ribs', 1 * FACTOR],
+  ['belly', 1 * FACTOR],
+  ['shoulder', 0.75 * FACTOR],
+  ['upper_arm', 1.25 * FACTOR],
+  ['lower_arm', 1 * FACTOR],
+  ['hand_half_height', 0.3 * FACTOR],
+  ['hand_half_width', 0.2 * FACTOR],
+  ['hip', 0.5 * FACTOR],
+  ['upper_leg', 1.5 * FACTOR],
+  ['lower_leg', 1.5 * FACTOR],
+  ['foot_half_height', 0.15 * FACTOR],
+  ['foot_half_width', 0.3 * FACTOR],
 ]);
 
 // Directions in Pi radians.
-const RIGHT = 0.0;
-const UP = 0.5;
-const LEFT = 1.0;
-const DOWN = 1.5;
+const DIRECTIONS = new Map([
+  ['right', 0.0],
+  ['16th', 0.125],
+  ['up', 0.5],
+  ['left', 1.0],
+  ['down', 1.5]
+]);
 
 /**
 Return an SVG element for a line.
@@ -92,83 +95,75 @@ class StickFigure {
 
   svg_close() {
     return [
-      "</g>",
-      "</svg>"
+      '</g>',
+      '</svg>'
     ];
   }
 
   // Side Effect! Sets `shoulder_nexus`
   torso() {
-    const belly_end = this.hip_nexus.polar_add(UP, BODY_LENGTHS.get("belly"));
-    const ribs_end = belly_end.polar_add(UP, BODY_LENGTHS.get("ribs"));
+    const belly_end = this.hip_nexus.polar_add(DIRECTIONS.get("up"), BODY_LENGTHS.get("belly"));
+    const ribs_end = belly_end.polar_add(DIRECTIONS.get("up"), BODY_LENGTHS.get("ribs"));
     this.shoulder_nexus = ribs_end;
     return [
-      svg_line(this.hip_nexus, belly_end), // belly
-      svg_line(belly_end, ribs_end) // ribs
+      svg_line(this.hip_nexus, belly_end),
+      svg_line(belly_end, ribs_end)
     ];
   }
 
   neck_head() {
-    const neck_end = this.shoulder_nexus.polar_add(UP, BODY_LENGTHS.get("neck"));
+    const neck_end = this.shoulder_nexus.polar_add(DIRECTIONS.get('up'), BODY_LENGTHS.get('neck'));
     return [
       svg_line(this.shoulder_nexus, neck_end),
-      offset_ellipse(neck_end, UP, BODY_LENGTHS.get("head_half_width"), BODY_LENGTHS.get("head_half_height"))
+      offset_ellipse(neck_end, DIRECTIONS.get('up'), BODY_LENGTHS.get('head_half_width'), BODY_LENGTHS.get('head_half_height'))
     ];
   }
 
   right_arm() {
-    const shoulder_end = this.shoulder_nexus.polar_add(RIGHT, BODY_LENGTHS.get("shoulder"));
-    const upper_arm_end = shoulder_end.polar_add(DOWN, BODY_LENGTHS.get("upper_arm"));
-    const lower_arm_end = upper_arm_end.polar_add(DOWN, BODY_LENGTHS.get("lower_arm"));
+    const shoulder_end = this.shoulder_nexus.polar_add(DIRECTIONS.get('right'), BODY_LENGTHS.get('shoulder'));
+    const upper_arm_end = shoulder_end.polar_add(DIRECTIONS.get('down'), BODY_LENGTHS.get('upper_arm'));
+    const lower_arm_end = upper_arm_end.polar_add(DIRECTIONS.get('down'), BODY_LENGTHS.get('lower_arm'));
     return [
       svg_line(this.shoulder_nexus, shoulder_end),
       svg_line(shoulder_end, upper_arm_end),
       svg_line(upper_arm_end, lower_arm_end),
-      offset_ellipse(lower_arm_end, DOWN, BODY_LENGTHS.get("hand_half_width"), BODY_LENGTHS.get("hand_half_height"))
+      offset_ellipse(lower_arm_end, DIRECTIONS.get('down'), BODY_LENGTHS.get('hand_half_width'), BODY_LENGTHS.get('hand_half_height'))
     ];
   }
 
   left_arm() {
-    const shoulder_end = this.shoulder_nexus.polar_add(LEFT, BODY_LENGTHS.get("shoulder"));
-    const upper_arm_end = shoulder_end.polar_add(DOWN, BODY_LENGTHS.get("upper_arm"));
-    const lower_arm_end = upper_arm_end.polar_add(DOWN, BODY_LENGTHS.get("lower_arm"));
+    const shoulder_end = this.shoulder_nexus.polar_add(DIRECTIONS.get('left'), BODY_LENGTHS.get('shoulder'));
+    const upper_arm_end = shoulder_end.polar_add(DIRECTIONS.get('down'), BODY_LENGTHS.get('upper_arm'));
+    const lower_arm_end = upper_arm_end.polar_add(DIRECTIONS.get('down'), BODY_LENGTHS.get('lower_arm'));
     return [
       svg_line(this.shoulder_nexus, shoulder_end),
       svg_line(shoulder_end, upper_arm_end),
       svg_line(upper_arm_end, lower_arm_end),
-      offset_ellipse(lower_arm_end, DOWN, BODY_LENGTHS.get("hand_half_width"), BODY_LENGTHS.get("hand_half_height"))
+      offset_ellipse(lower_arm_end, DIRECTIONS.get('down'), BODY_LENGTHS.get('hand_half_width'), BODY_LENGTHS.get('hand_half_height'))
     ];
   }
 
   right_leg() {
-    // right_hip
-    // right_upper_leg
-    // right_lower_leg
-    // right_foot
-    const hip_end = this.hip_nexus.polar_add(RIGHT, BODY_LENGTHS.get("hip"));
-    const upper_leg_end = hip_end.polar_add(DOWN, BODY_LENGTHS.get("upper_leg"));
-    const lower_leg_end = upper_leg_end.polar_add(DOWN, BODY_LENGTHS.get("lower_leg"));
+    const hip_end = this.hip_nexus.polar_add(DIRECTIONS.get('right'), BODY_LENGTHS.get('hip'));
+    const upper_leg_end = hip_end.polar_add(DIRECTIONS.get('down'), BODY_LENGTHS.get('upper_leg'));
+    const lower_leg_end = upper_leg_end.polar_add(DIRECTIONS.get('down'), BODY_LENGTHS.get('lower_leg'));
     return [
       svg_line(this.hip_nexus, hip_end),
       svg_line(hip_end, upper_leg_end),
       svg_line(upper_leg_end, lower_leg_end),
-      offset_ellipse(lower_leg_end, DOWN, BODY_LENGTHS.get("foot_half_width"), BODY_LENGTHS.get("foot_half_height"))
+      offset_ellipse(lower_leg_end, DIRECTIONS.get('down'), BODY_LENGTHS.get('foot_half_width'), BODY_LENGTHS.get('foot_half_height'))
     ];
   }
 
   left_leg() {
-    // left_hip
-    // left_upper_leg
-    // left_lower_leg
-    // left_foot
-    const hip_end = this.hip_nexus.polar_add(LEFT, BODY_LENGTHS.get("hip"));
-    const upper_leg_end = hip_end.polar_add(DOWN, BODY_LENGTHS.get("upper_leg"));
-    const lower_leg_end = upper_leg_end.polar_add(DOWN, BODY_LENGTHS.get("lower_leg"));
+    const hip_end = this.hip_nexus.polar_add(DIRECTIONS.get('left'), BODY_LENGTHS.get('hip'));
+    const upper_leg_end = hip_end.polar_add(DIRECTIONS.get('down'), BODY_LENGTHS.get('upper_leg'));
+    const lower_leg_end = upper_leg_end.polar_add(DIRECTIONS.get('down'), BODY_LENGTHS.get('lower_leg'));
     return [
       svg_line(this.hip_nexus, hip_end),
       svg_line(hip_end, upper_leg_end),
       svg_line(upper_leg_end, lower_leg_end),
-      offset_ellipse(lower_leg_end, DOWN, BODY_LENGTHS.get("foot_half_width"), BODY_LENGTHS.get("foot_half_height"))
+      offset_ellipse(lower_leg_end, DIRECTIONS.get('down'), BODY_LENGTHS.get('foot_half_width'), BODY_LENGTHS.get('foot_half_height'))
     ];
   }
 }
